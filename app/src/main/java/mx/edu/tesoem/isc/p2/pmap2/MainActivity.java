bfd11dab00dfff5c946770cc1338fc3b2108b49e
Pedro Meza Alarcon <PeterMA1098>
2019-10-03 23:35:10 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p2/pmap2/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p2.pmap2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
